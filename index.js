1be6706e8db986759f4213c05e7c56b4ffdeb1fb
Ksena <oksishukh@yahoo.com>
2017-12-15 09:14:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Ksena"
-var height = "74"
+var age = "20"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
